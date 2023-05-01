6690ce1c905963f3ea38a60801a278bd30e1b04b
kdh6194 <kdh6194@naver.com>
2023-05-01 16:07:55 +0900

    회원가입 실명확인 페이지

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -1,16 +1,16 @@
-let logo = document.querySelector("#logo");
+const logo = document.querySelector("#logo");
 logo.addEventListener('click', ()=>{
     location.href="/";
 });
 
 // 이용약관 체크박스 이벤트
-let agree1 = document.querySelector("#agree1");
+const agree1 = document.querySelector("#agree1");
 
 // 이용약관 체크박스 이벤트
-let agree2 = document.querySelector("#agree2");
+const agree2 = document.querySelector("#agree2");
 
 //개인정보 수집 동의
-let okagree = document.querySelector("#okagree");
+const okagree = document.querySelector("#okagree");
 okagree?.addEventListener('click', ()=>{
     if (!agree1.checked) {alert("이용약관에 동의를 체크하세요")}
     else if (!agree2.checked) {alert("개인정보 수집에 동의를 체크하세요")}
@@ -18,7 +18,29 @@ okagree?.addEventListener('click', ()=>{
 })
 
 //개인정보 수집 미동의
-let noagree = document.querySelector("#noagree");
-noagree.addEventListener('click', ()=>{
+const noagree = document.querySelector("#noagree");
+noagree?.addEventListener('click', ()=>{
     location.href="/";
 });
+
+//---------------------------------------------------
+
+// 실명확인
+const name2 = document.querySelector("#name2")
+const jumin1 = document.querySelector("#jumin1")
+const jumin2 = document.querySelector("#jumin2")
+const chkjumin = document.querySelector("#chkjumin")
+const chkbtn2 = document.querySelector("#check2btn")
+const checkfrm2 = document.querySelector("#checkfrm2")
+chkbtn2?.addEventListener('click',()=>{
+    if(name2.value == ''){alert("이름을 입력하세요")}
+    else if(jumin1.value == ''){alert("생년월일을 입력하세요")}
+    else if(jumin2.value == ''){alert("주민번호 뒷자리를 입력하세요")}
+    else if(!chkjumin.checked){alert("동의에 체크해주세요")}
+    else {
+        checkfrm2.method = 'post';
+        checkfrm2.action = "/join/joinme";
+        checkfrm2.submit();
+    }
+
+})
